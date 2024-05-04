3492e28b039b73f8af05b5c47347b24bd7371b07
Guilherme Melo <meloguilherme1994@gmail.com>
2024-05-04 11:05:14 -0300

    Linked List implementation

---
+++ src/main/java/io/github/guilhermemelo01/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,47 @@
+package io.github.guilhermemelo01.linkedlist;
+
+public class SinglyLinkedList {
+
+    public static void main(String[] args) {
+        Node<Integer> node2 = new Node<>(19);
+        Node<Integer> node1 = new Node<>(12);
+
+        node1.setNext(node2);
+
+        System.out.println(node1);
+    }
+
+    public static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data){
+            this.data = data;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "data=" + data +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+}
