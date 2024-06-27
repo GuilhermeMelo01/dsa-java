ef9f944047cd38a81ea446e829a3dda86620b8d7
Guilherme Melo <meloguilherme1994@gmail.com>
2024-06-27 18:49:59 -0300

    DSA - Delete Middle Node and Find Nth Node From End Of The List

--- src/main/java/io/github/guilhermemelo01/Main.java
+++ src/main/java/io/github/guilhermemelo01/Main.java
@@ -1,59 +1,23 @@
 package io.github.guilhermemelo01;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
 
-        char character = 'c';
-        String nome = "Cris";
-        int idade = 22;
-        double kilos = 120.0;
-        boolean verdade = true;
+        int tests = in.nextInt();
+        while(tests >= 0){
+            String value = in.nextLine();
+            String response = value;
 
-        /**
-         * JAVA
-         * - Backend <-> Frontend
-         * - Parte escondido - faz conexão bd
-         * - Typagem - int, string ou etc
-         * - PQ VAMOS APRENDER JAVA? MORE MONEY
-         * */
+            int size = value.length();
+            if(size > 10){
+                response = value.charAt(0) +""+ (size - 2) +""+ value.charAt(size - 1);
+            }
 
-        // JAVA
-        // LOGICA DE PROGRAMAÇÃO COM JAVA, POO, BANCO DE DADOS,
-
-        // 1 - PASSO -> ENVIAR CURRICULOS
-
-        // programacao -> ingles -> aplicar vaga exterior
-        // WEB -> java, python, javascript, c++, c#, html, css, ...
-
-        //Intellij e vscode -> criadores de codigo.
-        //linguagem + idea
-
-        // faculdade -> aprende uma linguagem qualquer -> aplica // 3 meses
-
-        //GITHUB - GIT
-
-        // GIT - LOCAL
-        // GITHUB - NUVEM
-
-        //
-
-        /**
-         * JAVASCRIPT
-         * - Frontend
-         * - Parte visivel - sem perigo
-         * - nao tipado - let, const
-         * */
-
-
-        /**
-         * ENGLISH
-         * - READ / LISTEN - VIDEO, SERIE, OU ALGO DO TIPO (vocabulario)
-         * - WRITE -
-         * - SPEAK -
-         *
-         * */
-
-        System.out.println(nome);
-        System.out.println(idade);
+            System.out.println(response);
+            tests--;
+        }
     }
 }
--- src/main/java/io/github/guilhermemelo01/linkedlist/base/SinglyLinkedList.java
+++ src/main/java/io/github/guilhermemelo01/linkedlist/base/SinglyLinkedList.java
@@ -1,5 +1,11 @@
 package io.github.guilhermemelo01.linkedlist.base;
 
+import io.github.guilhermemelo01.linkedlist.problems.listnode.ListNode;
+
+import javax.swing.*;
+import java.net.Inet4Address;
+import java.util.List;
+
 public class SinglyLinkedList {
 
     public static void main(String[] args) {
@@ -38,9 +44,247 @@ public class SinglyLinkedList {
         printValuesNode(head);
 
         System.out.println("----DELETE FROM GIVEN POSITION----");
-        head = deleteNodeFromGivenPosition(head, 5);
+        head = deleteNodeFromGivenPositionDummyHead(head, 1);
         printValuesNode(head);
+
+        System.out.println("----DELETE FROM KEY ----");
+        head = deleteNodeFromGivenValue(head, 10);
+        printValuesNode(head);
+
+        Node<Integer> headSorted = new Node<>(1);
+        Node<Integer> firstSorted = new Node<>(2);
+        Node<Integer> secondSorted = new Node<>(2);
+        Node<Integer> thirdSorted = new Node<>(10);
+        Node<Integer> fourSorted = new Node<>(21);
+        headSorted.setNext(firstSorted);
+        firstSorted.setNext(secondSorted);
+        secondSorted.setNext(thirdSorted);
+        thirdSorted.setNext(fourSorted);
+
+//        System.out.println("----INSERT INSIDE OF A SORTED LINKED LIST ----");
+//        headSorted = insertNodeInSortedLinkedList(headSorted, new Node<>(10));
+//        printValuesNode(headSorted);
+//
+//        System.out.println("----REMOVE DUPLICATES NODES FROM SORTED LINKED LIST ----");
+//        headSorted = removeDuplicateNodesFromSortedLinkedList(headSorted);
+//        printValuesNode(headSorted);
+//
+//        System.out.println("----SEARCH VALUE IN LINKED LIST ----");
+//        boolean exist = searchValueInLinkedList(headSorted, 3);
+//        System.out.println(exist);
+//
+//        System.out.println("----REVERSED LINKED LIST ----");
+//        Node<Integer> reversedLinkedList = reversedLinkedList(headSorted);
+//        printValuesNode(reversedLinkedList);
+//
+//        System.out.println("----LAST IN THE LINKED LIST ----");
+//        Node<Integer> last = lastInTheList(headSorted);
+//        printValuesNode(last);
+
+        System.out.println("---------------");
+        printValuesNode(headSorted);
+
+//        System.out.println("----MIDDLE NODE IN THE LINKED LIST ----");
+//        Node<Integer> findMiddleNode = findMiddleNode(headSorted);
+//        printValuesNode(findMiddleNode);
+
+        System.out.println("----FIND NTH NODE IN THE LINKED LIST ----");
+        Node<Integer> findNth = findNthNodeFromEndOfTheList(headSorted, 4);
+        printValuesNode(findNth);
     }
+
+    public static Node<Integer> lastInTheList(Node<Integer> head){
+
+        if (head == null){
+            return null;
+        }
+
+        Node<Integer> current = head;
+        Node<Integer> previous = null;
+
+        while(current != null){
+            previous = current;
+            current = current.next;
+
+        }
+
+        return previous;
+    }
+
+    public static Node<Integer> findMiddleNode(Node<Integer> head){
+
+        if (head == null){
+            return null;
+        }
+
+        if (head.next == null){
+            return head;
+        }
+
+        Node<Integer> slow = head, fast = head.next;
+        while(fast != null && fast.next != null && slow != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+
+    }
+
+    public static Node<Integer> findNthNodeFromEndOfTheList(Node<Integer> head, int position){
+
+        if (head == null){
+            return null;
+        }
+
+        if (head.next == null){
+            return head;
+        }
+
+        /*
+         * I could do the follow thing:
+         * I will take the position and do awhile until position is equal to 0, and point this node to fast.
+         * Now, I'm gonna take the node slow = head, node fast and node previous that will stay behind node slow.
+         * When the fast node be equal to null, I'm gonna take previous.next to slow.next and return head.
+         *
+         * */
+
+        Node<Integer> fast = head;
+        while(fast != null && position != 0){
+            fast = fast.next;
+            position--;
+        }
+
+        Node<Integer> slow = head;
+
+        while(fast != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+
+    }
+
+
+
+    public static Node<Integer> reversedLinkedList(Node<Integer> head){
+
+        if (head == null){
+            return null;
+        }
+
+        Node<Integer> current = head;
+        Node<Integer> next = null;
+        Node<Integer> previous = null;
+
+        /**
+         * <- 1 <- 2 <- 4 <- 5
+         *                     c
+         *                     n
+         *                   p -> RETURN
+         * c.next = 1
+         * */
+
+       while(current != null){
+           next = current.next;
+           current.next = previous;
+           previous = current;
+           current = next;
+       }
+
+        return previous;
+    }
+
+
+    public static boolean searchValueInLinkedList(Node<Integer> head, int value){
+        if (head == null){
+            return false;
+        }
+
+        Node<Integer> current = head;
+        while(current != null){
+            if (current.data == value){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public static Node<Integer> removeDuplicateNodesFromSortedLinkedList(Node<Integer> head){
+        if (head == null){
+            return null;
+        }
+        /** newNode = 3
+         *   w
+         * 1 2 3 3 null
+         *          c
+         *     p    n
+         * */
+
+        Node<Integer> current = head;
+        Node<Integer> previous = null;
+        while(current != null){
+            if (previous != null && current.data == previous.data){
+                previous.next = current.next;
+                current = current.next;
+            }else{
+                previous = current;
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    public static Node<Integer> insertNodeInSortedLinkedList(Node<Integer> head, Node<Integer> newNode){
+            if (head == null){
+                return null;
+            }
+
+            /** newNode = 3
+             *   w
+             * 1 2 5 6
+             *     c
+             *   t
+             * */
+            Node<Integer> current = head;
+            Node<Integer> temp = null;
+            while (current.data != null && current.data < newNode.data){
+                temp = current;
+                current = current.next;
+            }
+            temp.next = newNode;
+            newNode.next = current;
+            return head;
+    }
+
+    public static Node<Integer> deleteNodeFromGivenValue(Node<Integer> head, int value) {
+        if (head == null) {
+            return null;
+        }
+
+        Node<Integer> current = head;
+        Node<Integer> previous = null;
+        if (current.data == value) {
+           return current.next;
+        } else {
+            while (current.next != null) {
+                Node<Integer> temp = current;
+                if (current.data == value) {
+                    previous.next = current.next;
+                    current = previous;
+                    break;
+                } else {
+                    current = current.next;
+                    previous = temp;
+                }
+            }
+        }
+
+        return head;
+    }
+
 
     public static Node<Integer> deleteNodeFromGivenPosition(Node<Integer> head, int position) {
         if (head == null) {
@@ -62,6 +306,26 @@ public class SinglyLinkedList {
         }
 
         return head;
+    }
+
+    public static Node<Integer> deleteNodeFromGivenPositionDummyHead(Node<Integer> head, int position) {
+        // 1- Create a dummy head with -1 value and point next to head
+        // 2- We've to create a left pointer = head and right pointer that is equal to position
+        // 3- We've to create a loop until the right pointer == null
+        // 4- When the right pointer is null, we point the left.next pointer to right.next;
+        // 5- return the dummy.next
+
+        Node<Integer> dummy = new Node<>(-1, head);
+        Node<Integer> right = head;
+
+        while(position > 0 && right != null){
+            right = right.next;
+            position--;
+        }
+
+        dummy.next = dummy.next.next;
+
+        return dummy.next;
     }
 
     public static Node<Integer> deleteLastNode(Node<Integer> head) {
@@ -161,6 +425,11 @@ public class SinglyLinkedList {
 
         public Node(T data) {
             this.data = data;
+        }
+
+        public Node(T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
         }
 
         public T getData() {
