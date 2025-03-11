735a23698c8dbc2cadfc67828ffc65ee9ff9802f
GuilhermeMelo01 <meloguilherme1994@gmail.com>
2025-03-11 19:47:27 -0300

    remove vowels using set

--- src/main/java/io/github/guilhermemelo01/roadmap/computer_science/hashing/base/HashTable.java
+++ src/main/java/io/github/guilhermemelo01/roadmap/computer_science/hashing/base/HashTable.java
@@ -1,5 +1,7 @@
 package io.github.guilhermemelo01.roadmap.computer_science.hashing.base;
 
+import java.util.NoSuchElementException;
+
 public class HashTable {
     private static HashNode<Integer, String>[] buckets = new HashNode[10]; // 10 is my capacity
 
@@ -8,23 +10,75 @@ public class HashTable {
         add(new HashNode<>(110, "Gui"));
         add(new HashNode<>(11, "Ana"));
         add(new HashNode<>(1110, "Ana"));
+        add(new HashNode<>(1110, "Vitoria"));
+
+        remove(11);
+
+        System.out.println(get(1110));
 
         for (HashNode bucket : buckets) {
             System.out.println(bucket);
         }
     }
 
+    public static String get(Integer chave){
+        if (chave == null){
+            throw new NullPointerException("Chave está nula");
+        }
+        Integer index = hashFunction(chave);
+        HashNode<Integer, String> bucket = buckets[index];
+        while(bucket != null){
+            if(bucket.getChave().equals(chave)){
+                return bucket.getValor();
+            }
+            bucket = bucket.next;
+        }
+
+        throw new NoSuchElementException("Elemento com a chave: "+ chave + " não foi encontrado");
+    }
+
     public static void add(HashNode node) {
         Integer index = hashFunction((Integer) node.getChave());
+
+        HashNode<Integer, String> bucket = buckets[index];
+        while (bucket != null){
+            if(bucket.getChave().equals(node.getChave())){
+                bucket.setValor((String) node.getValor());
+                return;
+            }
+            bucket = bucket.next;
+        }
+
         if (buckets[index] == null) {
             buckets[index] = node;
         } else {
-            HashNode<Integer, String> bucket = buckets[index];
-            while (bucket.next != null) {
-                bucket = bucket.next;
-            }
-            bucket.next = node;
+            node.next = buckets[index];
+            buckets[index] = node;
         }
+    }
+
+    public static HashNode remove(Integer chave){
+        Integer index = hashFunction(chave);
+        HashNode<Integer, String> bucket = buckets[index];
+
+        HashNode previous = bucket;
+        HashNode atual = bucket.next;
+
+        if (previous.getChave().equals(chave)){
+            buckets[index] = atual;
+            return previous;
+        }
+
+        while(atual != null){
+            if(atual.getChave().equals(chave)){
+                previous.next = atual.next;
+                return atual;
+            }
+            previous = atual;
+            atual = atual.next;
+        }
+
+        throw new IllegalArgumentException("Node não foi encontrado");
     }
 
     private static Integer hashFunction(Integer chave) {
